2289eb90681447250090a1ee078d378ee318b14c
slw515 <slw515@nyu.edu>
2021-01-01 14:29:11 +0800

    allows fetching of objects from database through PullLongLat.cs script

---
+++ Assets/Scripts/PostCreation/PullLongLat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullLongLat : MonoBehaviour
+{
+  ArrayList arlist = new ArrayList();
+  public void Awake()
+  {
+    if (DBManager.username == null)
+    {
+      UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+    StartCoroutine(PullLongLatFromCreationTable());
+  }
+
+  IEnumerator PullLongLatFromCreationTable()
+  {
+    WWWForm form = new WWWForm();
+
+    WWW www = new WWW("http://stevenwyks.com/pullLongLatFromDB.php", form);
+    Debug.Log("heya!");
+
+    yield return www;
+    Debug.Log("DSadas");
+    Debug.Log(www.text);
+
+    // yield return currIndex;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
